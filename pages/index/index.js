52ea8bad02de0e99cac28c338c2646155a94ff2e
张阳 <916103324@qq.com>
2019-12-06 15:08:29 +0800

    第一次提交

---
+++ pages/index/index.js
@@ -0,0 +1,16 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    background: ['#222', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 2000,
+    duration: 500,
+    active:0
+  },
+  
+})
